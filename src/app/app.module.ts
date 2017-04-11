bed5cb65e44522bd0fb20962c9aafedbcb48e695
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-04-11 10:38:11 -0700

    Add basic Angular 2 and D3 setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { D3Service } from 'd3-ng2-service';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [D3Service],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { D3Service, D3, Selection, ScaleLinear } from 'd3-ng2-service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  private d3: D3;
+  private parentNativeElement: any;
+
+  constructor(element: ElementRef, d3Service: D3Service) {
+    this.d3 = d3Service.getD3();
+    this.parentNativeElement = element.nativeElement;
+  }
+
+  ngOnInit() {
+    let d3 = this.d3;
+    let d3ParentElement: Selection<any, any, any, any>;
+
+    if (this.parentNativeElement !== null) {
+      d3ParentElement = d3.select(this.parentNativeElement);
+    }
+  }
+
+}
